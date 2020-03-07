ef95a5d92c4612cd488779fc96b508884bf063d6
Erfan Abdi <erfangplus@gmail.com>
2020-03-07 05:30:13 +0330

    platina: Don’t enforce rro for all overlays
    * Breaking /product/overlay
    * Also this makes many platform specific overlays with current system API on vendor partition which breaks newer android version compatibilities (broken stuffs in gsis)
    
    Change-Id: I45eb18b0754726fb4f779521a0245dfcb1259b17

--- device.mk
+++ device.mk
@@ -301,8 +301,6 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(DEVICE_PATH)/overlay \
     $(DEVICE_PATH)/overlay-lineage
 
-PRODUCT_ENFORCE_RRO_TARGETS := *
-
 # Permissions
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/system-privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml \
