505851d4be4c078cfeef1a87d244f5287d9c6ebd
SakilMondal <mondalsakil@gmail.com>
2020-06-21 13:49:47 +0530

    platina: Disable default color mode
    * Changing display default color mode property value from 1 to 0.
    
    Change-Id: Id15914127751b0bcc5347743aabdfa0c480379a3
    Signed-off-by: SakilMondal <mondalsakil@gmail.com>

--- properties.mk
+++ properties.mk
@@ -158,7 +158,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.protected_contents=true \
     ro.vendor.display.cabl=0 \
     vendor.display.disable_skip_validate=1 \
-    vendor.display.enable_default_color_mode=1 \
+    vendor.display.enable_default_color_mode=0 \
     vendor.gralloc.enable_fb_ubwc=1
 
 # FRP
