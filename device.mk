f137c6910202f48cea901bbff5c7cfa1cc19a3da
etahamad <etahamad@icloud.com>
2021-03-19 08:59:18 +0200

    platina: Introduce IORap
    Considering how it's been working fine for the past 6 months and has demonstrated improvements to app launch time [1], let's
    re-enable it here.
    
    [1] https://medium.com/androiddevelopers/improving-app-startup-with-i-o-prefetching-62fbdb9c9020
    
    Co-authored-by: raiyanbinmohsin <raiyanbinmohsinshishir@gmail.com>
    Co-authored-by: DillerOFire <niktofe1@gmail.com>
    Co-authored-by: Nikhil Baddi <nikhilbaddi98@gmail.com>
    Signed-off-by: Hanif Ardhani <hanif.ardhani98@gmail.com>

--- device.mk
+++ device.mk
@@ -228,6 +228,10 @@ PRODUCT_PACKAGES += \
     init.target.rc \
     ueventd.qcom.rc
 
+# IORap
+PRODUCT_PACKAGES += \
+    iorap-app-startup-runner
+
 # IRQ
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
