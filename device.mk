e4062b51361a227e5054a37b299237bc5bc58754
OdSazib <odsazib@gmail.com>
2021-03-22 02:42:53 +0600

    platina: Build wfd dependency blobs from source
    * Avoid manifest entry for old blobs compatibility
    * Must use display hal from https://github.com/AOSP-11
    
    Signed-off-by: Hanif Ardhani <hanif.ardhani98@gmail.com>

--- device.mk
+++ device.mk
@@ -116,6 +116,9 @@ PRODUCT_PACKAGES += \
     hwcomposer.sdm660 \
     memtrack.sdm660 \
     libdisplayconfig \
+    libdisplayconfig.qti \
+    libdisplayconfig.qti.vendor \
+    libstagefright_enc_common \
     libtinyxml \
     libqdMetaData \
     libqdMetaData.system
@@ -130,7 +133,8 @@ PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-service \
     android.hardware.renderscript@1.0-impl \
     android.frameworks.displayservice@1.0 \
-    vendor.display.config@1.1
+    vendor.display.config@2.0 \
+    vendor.display.config@2.0.vendor
 
 # Display Calibration
 PRODUCT_COPY_FILES += \
