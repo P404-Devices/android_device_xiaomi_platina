149cf6dee4e313264c3f9a52065136b30dbc5225
fakeyatogod <fakeyatokami@gmail.com>
2021-04-07 16:00:48 +0530

    platina: Use Userspace LMKD if there's no LMK driver
    Signed-off-by: Hanif Ardhani <hanif.ardhani98@gmail.com>

--- properties.mk
+++ properties.mk
@@ -185,6 +185,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.iorapd.enable=true
 
+# LMKD options
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.config.low_ram=false \
+    ro.lmk.log_stats=true \
+    ro.lmk.use_psi=false
+
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.stagefright.omx_default_rank.sw-audio=1 \
